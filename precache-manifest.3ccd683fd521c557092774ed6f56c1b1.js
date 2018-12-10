5ff0f43b4806cc45976ecee63fb75d43587188ee
Coding <coding@Paulo.attlocal.net>
2018-12-10 13:44:55 -0500

    Updates

---
+++ precache-manifest.3ccd683fd521c557092774ed6f56c1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ClickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "929a7353197f4d6c748b",
+    "url": "/ClickyGame/static/js/main.929a7353.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/ClickyGame/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "929a7353197f4d6c748b",
+    "url": "/ClickyGame/static/css/main.7b40a776.chunk.css"
+  },
+  {
+    "revision": "3a6b1590c3704880ea3f26a031674533",
+    "url": "/ClickyGame/index.html"
+  }
+];
